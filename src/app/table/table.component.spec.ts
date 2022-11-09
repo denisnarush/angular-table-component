6dd12efbcae7374df179788816dd3c35700637f8
Denis Narush <child.denis@gmail.com>
2022-11-09 14:11:39 +0100

    updates setInput method on suites

--- src/app/table/table.component.spec.ts
+++ src/app/table/table.component.spec.ts
@@ -5,6 +5,7 @@ import { By } from '@angular/platform-browser';
 import { TableComponent } from './table.component';
 
 describe('TableComponent', () => {
+  const DELAY = 5;
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
   let rootDebugElement: DebugElement;
@@ -16,6 +17,8 @@ describe('TableComponent', () => {
   let emptyDataDebugElement: DebugElement;
   let emptyDataHTMLElement: HTMLElement;
 
+  let rowDebugElements: DebugElement[];
+
   function reCreate() {
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
@@ -24,18 +27,25 @@ describe('TableComponent', () => {
   function reInitElements() {
     rootDebugElement = fixture.debugElement.query(By.css('.table-container'));
     rootHtmlElement = rootDebugElement?.nativeElement;
+
     noConfigDebugElement = fixture.debugElement.query(
       By.css('[data-e2e="no-config')
     );
     noConfigHTMLElement = noConfigDebugElement?.nativeElement;
+
     noDataDebugElement = fixture.debugElement.query(
       By.css('[data-e2e="no-data')
     );
     noDataHTMLElement = noDataDebugElement?.nativeElement;
+
     emptyDataDebugElement = fixture.debugElement.query(
       By.css('[data-e2e="empty-data')
     );
     emptyDataHTMLElement = emptyDataDebugElement?.nativeElement;
+
+    rowDebugElements = fixture.debugElement.queryAll(
+      By.css('[data-e2e="data-row')
+    );
   }
 
   function init(beforeOnInit = () => {}) {
@@ -45,13 +55,22 @@ describe('TableComponent', () => {
     reInitElements();
   }
 
+  function setInput(name: string, value: unknown) {
+    fixture.componentRef.setInput(name, value);
+  }
+
   beforeEach(async () => {
+    jasmine.clock().install();
     await TestBed.configureTestingModule({
       declarations: [TableComponent],
     }).compileComponents();
 
     reCreate();
     fixture.detectChanges();
+  });
+
+  afterEach(async () => {
+    jasmine.clock().uninstall();
   });
 
   it('should create', () => {
@@ -74,7 +93,7 @@ describe('TableComponent', () => {
   it(`can be only input provided`, () => {
     init(() => {
       // @Input() config =
-      component.config = { columns: [], uniqIdKey: 'id' };
+      setInput('config', { columns: [], uniqIdKey: 'id' });
     });
 
     expect(rootDebugElement).toBeDefined();
@@ -90,10 +109,10 @@ describe('TableComponent', () => {
   it(`display 'no data' when config specified and data has not`, () => {
     init(() => {
       // @Input() config =
-      component.config = {
+      setInput('config', {
         columns: [{ alias: 'id', label: 'ID' }],
         uniqIdKey: 'id',
-      };
+      });
     });
 
     expect(rootDebugElement).toBeDefined();
@@ -109,12 +128,12 @@ describe('TableComponent', () => {
   it(`display 'no data' when config specified and data is null`, () => {
     init(() => {
       // @Input() config =
-      component.config = {
+      setInput('config', {
         columns: [{ alias: 'id', label: 'ID' }],
         uniqIdKey: 'id',
-      };
+      });
       // @Input() data =
-      component.data = null;
+      setInput('data', null);
     });
 
     expect(rootDebugElement).toBeDefined();
@@ -130,12 +149,12 @@ describe('TableComponent', () => {
   it(`display 'no data' when config specified and data is [<empty>]`, () => {
     init(() => {
       // @Input() config =
-      component.config = {
+      setInput('config', {
         columns: [{ alias: 'id', label: 'ID' }],
         uniqIdKey: 'id',
-      };
+      });
       // @Input() data =
-      component.data = [];
+      setInput('data', []);
     });
 
     expect(rootDebugElement).toBeDefined();
@@ -146,5 +165,70 @@ describe('TableComponent', () => {
     expect(noDataHTMLElement).toBeUndefined();
     expect(emptyDataDebugElement).toBeDefined();
     expect(emptyDataHTMLElement).toBeDefined();
+  });
+
+  it(`display row of statick data`, () => {
+    init(() => {
+      // @Input() config =
+      setInput('config', {
+        columns: [{ alias: 'id', label: 'ID' }],
+        uniqIdKey: 'id',
+      });
+      // @Input() data =
+      setInput('data', [
+        {
+          id: '31558be5-78f6-40d6-8c35-c94ebf36da99',
+        },
+      ]);
+    });
+
+    expect(rootDebugElement).toBeDefined();
+    expect(rootHtmlElement).toBeDefined();
+    expect(noConfigDebugElement).toBeNull();
+    expect(noConfigHTMLElement).toBeUndefined();
+    expect(noDataDebugElement).toBeNull();
+    expect(noDataHTMLElement).toBeUndefined();
+    expect(emptyDataDebugElement).toBeNull();
+    expect(emptyDataHTMLElement).toBeUndefined();
+    expect(rowDebugElements).toBeDefined();
+    expect(rowDebugElements.length).toBe(1);
+    expect(rowDebugElements[0].nativeElement).toBeDefined();
+  });
+
+  it(`display row of when async data`, () => {
+    init(() => {
+      // @Input() config =
+      setInput('config', {
+        columns: [{ alias: 'id', label: 'ID' }],
+        uniqIdKey: 'id',
+      });
+      // @Input() data =
+      setInput('data', null);
+    });
+
+    setTimeout(() => {
+      fixture.componentRef.setInput('data', [
+        {
+          id: '31558be5-78f6-40d6-8c35-c94ebf36da99',
+        },
+      ]);
+    }, DELAY);
+
+    jasmine.clock().tick(DELAY);
+    fixture.detectChanges();
+    reInitElements();
+
+    expect(rootDebugElement).toBeDefined();
+    expect(rootHtmlElement).toBeDefined();
+    expect(noConfigDebugElement).toBeNull();
+    expect(noConfigHTMLElement).toBeUndefined();
+    expect(noDataDebugElement).toBeNull();
+    expect(noDataHTMLElement).toBeUndefined();
+    expect(emptyDataDebugElement).toBeNull();
+    expect(emptyDataHTMLElement).toBeUndefined();
+
+    expect(rowDebugElements).toBeDefined();
+    expect(rowDebugElements.length).toBe(1);
+    expect(rowDebugElements[0].nativeElement).toBeDefined();
   });
 });
